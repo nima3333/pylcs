8498a99aa1852c58c0dc5143c1e9e2c3cc281cb7
Nima Alikhani <nimajaune@gmail.com>
2021-03-15 14:09:07 +0800

    format

--- tests/test_pylcs.py
+++ tests/test_pylcs.py
@@ -1,46 +1,47 @@
 # encoding=utf-8
-import pylcs
 import numpy as np
+import pylcs
 
 
 def test_lcs():
-    assert pylcs.lcs("aaa", "bbb") == 0
-    assert pylcs.lcs("aaa", "aabbbaa") == 3
-    assert pylcs.lcs("你好", "中国") == 0
-    assert pylcs.lcs("aaa你好", "你好呀") == 2
+	assert pylcs.lcs("aaa", "bbb") == 0
+	assert pylcs.lcs("aaa", "aabbbaa") == 3
+	assert pylcs.lcs("你好", "中国") == 0
+	assert pylcs.lcs("aaa你好", "你好呀") == 2
 
 
 def test_lcs_of_list():
-    assert pylcs.lcs_of_list("aaa", ["aabbbaa"] * 10) == [3] * 10
-    assert pylcs.lcs_of_list("aaa你好", ["你好呀"] * 10) == [2] * 10
+	assert pylcs.lcs_of_list("aaa", ["aabbbaa"] * 10) == [3] * 10
+	assert pylcs.lcs_of_list("aaa你好", ["你好呀"] * 10) == [2] * 10
 
 
 def test_lcs2():
-    assert pylcs.lcs2("aaa", "bbb") == 0
-    assert pylcs.lcs2("aaa", "aabbbaa") == 2
-    assert pylcs.lcs2("你好", "中国") == 0
-    assert pylcs.lcs2("aaa你好", "好呀你") == 1
+	assert pylcs.lcs2("aaa", "bbb") == 0
+	assert pylcs.lcs2("aaa", "aabbbaa") == 2
+	assert pylcs.lcs2("你好", "中国") == 0
+	assert pylcs.lcs2("aaa你好", "好呀你") == 1
 
 
 def test_lcs2_of_list():
-    assert pylcs.lcs2_of_list("aaa", ["aabbbaa"] * 10) == [2] * 10
-    assert pylcs.lcs2_of_list("aaa你好", ["好呀你"] * 10) == [1] * 10
+	assert pylcs.lcs2_of_list("aaa", ["aabbbaa"] * 10) == [2] * 10
+	assert pylcs.lcs2_of_list("aaa你好", ["好呀你"] * 10) == [1] * 10
 
 
 def test_edit_distance():
-    assert pylcs.edit_distance("aaa", "bbb") == 3
-    assert pylcs.edit_distance("aaa", "aabbbaa") == 4
-    assert pylcs.edit_distance("你好", "中国") == 2
-    assert pylcs.edit_distance("aaa你好", "你好呀") == 4
+	assert pylcs.edit_distance("aaa", "bbb") == 3
+	assert pylcs.edit_distance("aaa", "aabbbaa") == 4
+	assert pylcs.edit_distance("你好", "中国") == 2
+	assert pylcs.edit_distance("aaa你好", "你好呀") == 4
 
 
 def test_edit_distance_of_list():
-    assert pylcs.edit_distance_of_list("aaa", ["bbb"] * 10) == [3] * 10
-    assert pylcs.edit_distance_of_list("aaa你好", ["你好呀"] * 10) == [4] * 10
-    assert pylcs.edit_distance_of_list("aaa你好", ["bbb", "你好呀"] * 10) == [5, 4] * 10
+	assert pylcs.edit_distance_of_list("aaa", ["bbb"] * 10) == [3] * 10
+	assert pylcs.edit_distance_of_list("aaa你好", ["你好呀"] * 10) == [4] * 10
+	assert pylcs.edit_distance_of_list("aaa你好", ["bbb", "你好呀"] * 10) == [5, 4] * 10
 
-a = np.ones((10,4)) * 3 
-b = np.ones((10,4)) * 3 
+
+a = np.ones((10, 4)) * 3
+b = np.ones((10, 4)) * 3
 b[1][0] = 4
 c = pylcs.add_arrays(a, b)
 print(c)
